fed7c23223a1e1218ad2194a7af4d8f4b09ca4b0
taewoo <rlaxodn322@gmail.com>
2024-05-17 09:39:21 +0900

    1~9번 평균 최저 최대 편차

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -72,6 +72,54 @@ const Home = () => {
                 TrayCellMaxTemp3: any;
                 TrayCellMinTemp3: any;
                 TrayCellDifTemp3: any;
+                TrayCellAvgVolt4: any;
+                TrayCellMaxVolt4: any;
+                TrayCellMinVolt4: any;
+                TrayCellDifVolt4: any;
+                TrayCellAvgTemp4: any;
+                TrayCellMaxTemp4: any;
+                TrayCellMinTemp4: any;
+                TrayCellDifTemp4: any;
+                TrayCellAvgVolt5: any;
+                TrayCellMaxVolt5: any;
+                TrayCellMinVolt5: any;
+                TrayCellDifVolt5: any;
+                TrayCellAvgTemp5: any;
+                TrayCellMaxTemp5: any;
+                TrayCellMinTemp5: any;
+                TrayCellDifTemp5: any;
+                TrayCellAvgVolt6: any;
+                TrayCellMaxVolt6: any;
+                TrayCellMinVolt6: any;
+                TrayCellDifVolt6: any;
+                TrayCellAvgTemp6: any;
+                TrayCellMaxTemp6: any;
+                TrayCellMinTemp6: any;
+                TrayCellDifTemp6: any;
+                TrayCellAvgVolt7: any;
+                TrayCellMaxVolt7: any;
+                TrayCellMinVolt7: any;
+                TrayCellDifVolt7: any;
+                TrayCellAvgTemp7: any;
+                TrayCellMaxTemp7: any;
+                TrayCellMinTemp7: any;
+                TrayCellDifTemp7: any;
+                TrayCellAvgVolt8: any;
+                TrayCellMaxVolt8: any;
+                TrayCellMinVolt8: any;
+                TrayCellDifVolt8: any;
+                TrayCellAvgTemp8: any;
+                TrayCellMaxTemp8: any;
+                TrayCellMinTemp8: any;
+                TrayCellDifTemp8: any;
+                TrayCellAvgVolt9: any;
+                TrayCellMaxVolt9: any;
+                TrayCellMinVolt9: any;
+                TrayCellDifVolt9: any;
+                TrayCellAvgTemp9: any;
+                TrayCellMaxTemp9: any;
+                TrayCellMinTemp9: any;
+                TrayCellDifTemp9: any;
               };
             }) => ({
               time: item.time,
@@ -101,6 +149,54 @@ const Home = () => {
               TrayCellMaxTemp3: item.data.TrayCellMaxTemp3,
               TrayCellMinTemp3: item.data.TrayCellMinTemp3,
               TrayCellDifTemp3: item.data.TrayCellDifTemp3,
+              TrayCellAvgTemp4: item.data.TrayCellAvgTemp4,
+              TrayCellMaxTemp4: item.data.TrayCellMaxTemp4,
+              TrayCellMinTemp4: item.data.TrayCellMinTemp4,
+              TrayCellDifTemp4: item.data.TrayCellDifTemp4,
+              TrayCellAvgVolt4: item.data.TrayCellAvgVolt4,
+              TrayCellMaxVolt4: item.data.TrayCellMaxVolt4,
+              TrayCellMinVolt4: item.data.TrayCellMinVolt4,
+              TrayCellDifVolt4: item.data.TrayCellDifVolt4,
+              TrayCellAvgTemp5: item.data.TrayCellAvgTemp5,
+              TrayCellMaxTemp5: item.data.TrayCellMaxTemp5,
+              TrayCellMinTemp5: item.data.TrayCellMinTemp5,
+              TrayCellDifTemp5: item.data.TrayCellDifTemp5,
+              TrayCellAvgVolt5: item.data.TrayCellAvgVolt5,
+              TrayCellMaxVolt5: item.data.TrayCellMaxVolt5,
+              TrayCellMinVolt5: item.data.TrayCellMinVolt5,
+              TrayCellDifVolt5: item.data.TrayCellDifVolt5,
+              TrayCellAvgTemp6: item.data.TrayCellAvgTemp6,
+              TrayCellMaxTemp6: item.data.TrayCellMaxTemp6,
+              TrayCellMinTemp6: item.data.TrayCellMinTemp6,
+              TrayCellDifTemp6: item.data.TrayCellDifTemp6,
+              TrayCellAvgVolt6: item.data.TrayCellAvgVolt6,
+              TrayCellMaxVolt6: item.data.TrayCellMaxVolt6,
+              TrayCellMinVolt6: item.data.TrayCellMinVolt6,
+              TrayCellDifVolt6: item.data.TrayCellDifVolt6,
+              TrayCellAvgTemp7: item.data.TrayCellAvgTemp7,
+              TrayCellMaxTemp7: item.data.TrayCellMaxTemp7,
+              TrayCellMinTemp7: item.data.TrayCellMinTemp7,
+              TrayCellDifTemp7: item.data.TrayCellDifTemp7,
+              TrayCellAvgVolt7: item.data.TrayCellAvgVolt7,
+              TrayCellMaxVolt7: item.data.TrayCellMaxVolt7,
+              TrayCellMinVolt7: item.data.TrayCellMinVolt7,
+              TrayCellDifVolt7: item.data.TrayCellDifVolt7,
+              TrayCellAvgTemp8: item.data.TrayCellAvgTemp8,
+              TrayCellMaxTemp8: item.data.TrayCellMaxTemp8,
+              TrayCellMinTemp8: item.data.TrayCellMinTemp8,
+              TrayCellDifTemp8: item.data.TrayCellDifTemp8,
+              TrayCellAvgVolt8: item.data.TrayCellAvgVolt8,
+              TrayCellMaxVolt8: item.data.TrayCellMaxVolt8,
+              TrayCellMinVolt8: item.data.TrayCellMinVolt8,
+              TrayCellDifVolt8: item.data.TrayCellDifVolt8,
+              TrayCellAvgTemp9: item.data.TrayCellAvgTemp9,
+              TrayCellMaxTemp9: item.data.TrayCellMaxTemp9,
+              TrayCellMinTemp9: item.data.TrayCellMinTemp9,
+              TrayCellDifTemp9: item.data.TrayCellDifTemp9,
+              TrayCellAvgVolt9: item.data.TrayCellAvgVolt9,
+              TrayCellMaxVolt9: item.data.TrayCellMaxVolt9,
+              TrayCellMinVolt9: item.data.TrayCellMinVolt9,
+              TrayCellDifVolt9: item.data.TrayCellDifVolt9,
             }),
           );
           setTableData(formattedData);
@@ -155,6 +251,30 @@ const Home = () => {
       // 'TrayCellMaxVolt3',
       // 'TrayCellMinVolt3',
       // 'TrayCellDifVolt3',
+      // 'TrayCellAvgVolt4',
+      // 'TrayCellMaxVolt4',
+      // 'TrayCellMinVolt4',
+      // 'TrayCellDifVolt4',
+      // 'TrayCellAvgVolt5',
+      // 'TrayCellMaxVolt5',
+      // 'TrayCellMinVolt5',
+      // 'TrayCellDifVolt5',
+      // 'TrayCellAvgVolt6',
+      // 'TrayCellMaxVolt6',
+      // 'TrayCellMinVolt6',
+      // 'TrayCellDifVolt6',
+      // 'TrayCellAvgVolt7',
+      // 'TrayCellMaxVolt7',
+      // 'TrayCellMinVolt7',
+      // 'TrayCellDifVolt7',
+      // 'TrayCellAvgVolt8',
+      // 'TrayCellMaxVolt8',
+      // 'TrayCellMinVolt8',
+      // 'TrayCellDifVolt8',
+      // 'TrayCellAvgVolt9',
+      // 'TrayCellMaxVolt9',
+      // 'TrayCellMinVolt9',
+      // 'TrayCellDifVolt9',
       'TrayCellAvgTemp1',
       'TrayCellMaxTemp1',
       'TrayCellMinTemp1',
@@ -167,6 +287,30 @@ const Home = () => {
       // 'TrayCellMaxTemp3',
       // 'TrayCellMinTemp3',
       // 'TrayCellDifTemp3',
+      // 'TrayCellAvgTemp4',
+      // 'TrayCellMaxTemp4',
+      // 'TrayCellMinTemp4',
+      // 'TrayCellDifTemp4',
+      // 'TrayCellAvgTemp5',
+      // 'TrayCellMaxTemp5',
+      // 'TrayCellMinTemp5',
+      // 'TrayCellDifTemp5',
+      // 'TrayCellAvgTemp6',
+      // 'TrayCellMaxTemp6',
+      // 'TrayCellMinTemp6',
+      // 'TrayCellDifTemp6',
+      // 'TrayCellAvgTemp7',
+      // 'TrayCellMaxTemp7',
+      // 'TrayCellMinTemp7',
+      // 'TrayCellDifTemp7',
+      // 'TrayCellAvgTemp8',
+      // 'TrayCellMaxTemp8',
+      // 'TrayCellMinTemp8',
+      // 'TrayCellDifTemp8',
+      // 'TrayCellAvgTemp9',
+      // 'TrayCellMaxTemp9',
+      // 'TrayCellMinTemp9',
+      // 'TrayCellDifTemp9',
     ];
 
     const columnsToDisplay = columnSearch ? ['time', 'RackNumber', columnSearch] : allColumns;
@@ -234,7 +378,9 @@ const Home = () => {
             onChange={handleColumnSearchChange}
           />
         </div>
-        <div className="table-container">{renderTable()}</div>
+        <div style={{ margin: '0 auto', display: 'flex', justifyContent: 'center' }}>
+          <div className="table-container">{renderTable()}</div>
+        </div>
       </div>
     </Page>
   );
